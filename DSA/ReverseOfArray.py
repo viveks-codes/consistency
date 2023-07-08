2409799b9ca9907d48f125fe3d758221af476b12
Vivek Patel <55041874+viveks-codes@users.noreply.github.com>
2023-07-08 21:38:27 +0530

    reverse of string Day 1

---
+++ DSA/ReverseOfArray.py
@@ -0,0 +1,18 @@
+# Input  : arr[] = {1, 2, 3}
+# Output : arr[] = {3, 2, 1}
+
+# Input :  arr[] = {4, 5, 1, 2}
+# Output : arr[] = {2, 1, 5, 4}
+
+def reverseList(A, start, end):
+    while start < end:
+        A[start], A[end] = A[end], A[start]
+        start += 1
+        end -= 1
+  
+# Driver function to test above function
+A = [1, 2, 3, 4, 5, 6]
+print(A)
+reverseList(A, 0, 5)
+print("Reversed list is")
+print(A)
